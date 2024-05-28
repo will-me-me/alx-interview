cd791288a1a52057ba95eec6ce00b9f14ec6cc19
“willy <[email protected]>
2024-05-28 14:14:06 +0300

    a good old game of chess

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,7 +19,7 @@ n = int(sys.argv[1])
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+    """find possible positions"""
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
@@ -29,7 +29,7 @@ def queens(n, i=0, a=[], b=[], c=[]):
 
 
 def solve(n):
-    """ solve """
+    """solve"""
     k = []
     i = 0
     for solution in queens(n, 0):
