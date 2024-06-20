684883e306f0d995b8c765593e1bcba2fe14bdad
“willy <[email protected]>
2024-06-20 11:59:25 +0300

    updated the code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""ALX Interview - makeChange Module."""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    coins = sorted(coins, reverse=True)
     change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
             total -= coin
             change += 1
-    return change if (change > 0 and total == 0) else -1
+        if total == 0:
+            return change
+    return -1
